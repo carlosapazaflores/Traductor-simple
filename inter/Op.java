5e2cf539cf0b5b910a7d5a48f056acc2815f629a
genius22123 <marlon2212genius@gmail.com>
2022-05-13 15:56:52 -0500

    first commit

---
+++ inter/Op.java
@@ -0,0 +1,13 @@
+ package inter; // Archivo Op.java
+ import analizadorLexico.*; 
+ import symbols.*;
+ 
+ public class Op extends Expr {
+ public Op(Token tok, Tipo p) { super(tok, p); }
+ public Expr reducir() {
+ Expr x = gen();
+ Temp t = new Temp(tipo);
+ emitir( t.toString() + " = " + x.toString() );
+ return t;
+  }
+}
